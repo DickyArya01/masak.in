01bca1a6598d41919e7e8e5daa4fd7746755efaf
dickyarya <79236074+DickyArya01@users.noreply.github.com>
2022-11-28 17:11:08 +0700

    add fragment
    add home fragment

---
+++ app/src/main/java/com/dickyarya/masakin/home/HomeActivity.kt
@@ -0,0 +1,56 @@
+package com.dickyarya.masakin.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.Fragment
+import com.dickyarya.masakin.R
+import com.dickyarya.masakin.constant.homeText
+import com.dickyarya.masakin.databinding.ActivityHomeBinding
+
+class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
+    private lateinit var mToolbar: Toolbar
+    private lateinit var tvToolbarTitle: TextView
+
+    private lateinit var homeFragment: HomeFragment
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+
+        setuptoolbar(homeText)
+
+        homeFragment = HomeFragment()
+
+        addFragment(homeFragment)
+
+
+    }
+
+    private fun addFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.add(R.id.fragmentContainer, fragment)
+        fragmentTransaction.commit()
+    }
+
+    private fun setuptoolbar(title: String){
+        mToolbar = findViewById(R.id.home_toolbar)
+        tvToolbarTitle = findViewById(R.id.toolbar_title)
+
+        setSupportActionBar(mToolbar)
+
+        tvToolbarTitle.text = title
+
+    }
+
+
+}
